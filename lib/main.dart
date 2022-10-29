98d76d851c4a9296599d409b790c62d5ddf02d61
Val <valemapa@hotmail.com>
2022-10-29 09:12:27 -0500

    Login page - Valentina

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'pages/splash_page.dart';
 
 void main() {
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
 
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.workSansTextTheme() ,
       ),
       home: const SplashPage(),
     );
