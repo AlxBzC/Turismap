d8e6c304b63ca9dee268995c515d60f3f962734f
Valmad <111368879+Valmad@users.noreply.github.com>
2022-11-21 20:42:52 -0500

    Updated

---
+++ lib/models/sitioslocal.g.dart
@@ -0,0 +1,42 @@
+/*
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'sitioslocal.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SitiosLocalAdapter extends TypeAdapter<SitiosLocal> {
+  @override
+  final int typeId = 0;
+
+  @override
+  SitiosLocal read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SitiosLocal()
+      ..id = fields[0] as String?;
+  }
+
+  @override
+  void write(BinaryWriter writer, SitiosLocal obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.id);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SitiosLocalAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+*/
