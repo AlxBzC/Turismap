5ae474b3645b2c525cd8e1de8e34cdb9b5be0231
Valmad <111368879+Valmad@users.noreply.github.com>
2022-11-21 21:09:26 -0500

    Updated

---
+++ lib/repository/boxes.dart
@@ -0,0 +1,9 @@
+/*
+import 'package:hive/hive.dart';
+import 'package:registrar_page_turismapp/models/sitioslocal.dart';
+
+class Boxes{
+
+  static Box<SitiosLocal> boxFavoritos() => Hive.box<SitiosLocal>('favoritos');
+}
+*/
